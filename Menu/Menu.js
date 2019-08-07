110429e81bca529a2b018cde07e745df66d5b4f7
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2019-08-07 15:21:13 -0700

    nested the event listener inside the creator function

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -10,13 +10,10 @@ let menuItems = [
 ];
 
 
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
 
   let headerDiv = document.querySelector('.header');
   let mainMenu = document.querySelector('.header .menu-button');
-  mainMenu.addEventListener('click', function() {
-    finishedMenu.classList.toggle('menu--open');
-  });
 
   function menuCreator(array) {
     const menu = document.createElement('div');
@@ -27,7 +24,7 @@ window.addEventListener("load", function(){
     const menuTechTrends = document.createElement('li');
     const menuMusic = document.createElement('li');
     const menuLogOut = document.createElement('li');
-  
+
     menu.appendChild(menuUl);
     menuUl.appendChild(menuStudents);
     menuUl.appendChild(menuFaculty);
@@ -35,19 +32,23 @@ window.addEventListener("load", function(){
     menuUl.appendChild(menuTechTrends);
     menuUl.appendChild(menuMusic);
     menuUl.appendChild(menuLogOut);
-  
+
     menu.classList.add('menu');
-  
+
     menuStudents.textContent = array['0'];
     menuFaculty.textContent = array['1'];
     menuWhatsNew.textContent = array['2'];
     menuTechTrends.textContent = array['3'];
     menuMusic.textContent = array['4'];
     menuLogOut.textContent = array['5'];
-  
+
+    mainMenu.addEventListener('click', function () {
+      menu.classList.toggle('menu--open');
+    });
+
     return menu
   }
-
+  
   let finishedMenu = menuCreator(menuItems);
   headerDiv.appendChild(finishedMenu);
 
@@ -57,7 +58,7 @@ window.addEventListener("load", function(){
 
 
 
-/* 
+/*
 
   Step 1: Write a function that will create a menu component as seen below:
 
@@ -69,15 +70,15 @@ window.addEventListener("load", function(){
 
   Pass the function an array as it's only argument.
 
-  Step 2: Iterate over the array creating a list item <li> element for each item in the array. 
+  Step 2: Iterate over the array creating a list item <li> element for each item in the array.
   Add those items to the <ul>
 
   Step 3: Using a DOM selector, select the menu button currently on the DOM.
-  
+
   Step 4: add a click handler to the menu button, when clicked it should toggle the class 'menu--open' on the menu itself
 
   Step 5: return the menu component.
 
   Step 6: add the menu component to the DOM.
-  
+
 */
